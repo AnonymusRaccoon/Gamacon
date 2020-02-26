98389f9ae7225c5901e9ae76b701e8dd9f4d44f2
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-02-26 11:55:40 +0100

    added map rendering

--- include/components/vertex_component.h
+++ include/components/vertex_component.h
@@ -10,10 +10,27 @@
 #include "component.h"
 #include <stdbool.h>
 
+struct vertex
+{
+    int x;
+    int y;
+    int z;
+};
+
+struct tile
+{
+    char *texture_name;
+    struct vertex *corners[4];
+    //int *corners[4];
+    void *texture;
+};
+
 struct vertex_component
 {
     gc_component base;
-    int **vertices;
+    struct tile *map;
+    //int **vertices;
+    struct vertex **vertices;
 };
 
 extern const struct vertex_component vertex_component;
--- src/sfml_renderer/sfml_vertex.c
+++ src/sfml_renderer/sfml_vertex.c
@@ -14,28 +14,62 @@
 #include "components/vertex_component.h"
 #include <math.h>
 #include <stdint.h>
+#include <malloc.h>
 #include <SFML/Graphics.h>
-
-sfVertexArray *sfml_init_verticies(void)
-{
-    sfVertexArray *arr = sfVertexArray_create();
-
-    sfVertexArray_append(arr, (sfVertex){{0, 0}, sfWhite});
-    sfVertexArray_append(arr, (sfVertex){{0, 0}, sfWhite});
-    sfVertexArray_setPrimitiveType(arr, sfLinesStrip);
-    return (arr);
-}
 
 #define ANGLE_X 45
 #define ANGLE_Y 35
 
-sfVector2f get_tile_coords(int x, int y, int z)
+sfVertexArray *sfml_init_verticies(void)
 {
-//	return (sfVector2f) {x * 64, y * 64};
+    sfVertexArray *arr = sfVertexArray_create();
+    sfVertex v[4] = {
+		{.position = {0, 0}, .texCoords = {0, 0}, .color = sfWhite},
+		{.position = {0, 0}, .texCoords = {16, 0}, .color = sfWhite},
+		{.position = {0, 0}, .texCoords = {0, 16}, .color = sfWhite},
+		{.position = {0, 0}, .texCoords = {16, 16}, .color = sfWhite}
+	};
+
+    for (int i = 0; i < 4; i++)
+        sfVertexArray_append(arr, v[i]);
+    sfVertexArray_setPrimitiveType(arr, sfTriangleStrip);
+    return (arr);
+}
+
+sfVector2f get_tile_coords(int y, int x, int z)
+{
 	return (sfVector2f){
 		cos(ANGLE_X) * x * 64 - cos (ANGLE_X) * y * 64,
-		sin(ANGLE_Y) * y  * 64 + sin (ANGLE_Y) * x * 64 - z
+		(sin(ANGLE_Y) * y  * 64 + sin (ANGLE_Y) * x * 64 - z) + 250
 	};
+}
+
+void draw_tile(struct sfml_renderer_system *this, struct vertex_component \
+*info, struct tile tile)
+{
+	sfRenderStates *states = malloc(sizeof(sfRenderStates));
+    sfVertex *vert1 = sfVertexArray_getVertex(this->vertices, 1);
+    sfVertex *vert2 = sfVertexArray_getVertex(this->vertices, 2);
+    sfVertex *vert3 = sfVertexArray_getVertex(this->vertices, 3);
+	int corners[9];
+
+	if (tile.corners[0]->z == INT32_MAX || !states)
+		return;
+    if (!tile.corners[1]->y || !tile.corners[2]->y || !tile.corners[3]->y)
+        return;
+	for (int i = 0; i < 3; i++) {
+		corners[i * 3] = tile.corners[i + 1]->x;
+		corners[i * 3 + 1] = tile.corners[i + 1]->y;
+		corners[i * 3 + 2] = tile.corners[i + 1]->z;
+	}
+	vert1->position = get_tile_coords(corners[0], corners[1], corners[2]);
+    vert2->position = get_tile_coords(corners[3], corners[4], corners[5]);
+    vert3->position = get_tile_coords(corners[6], corners[7], corners[8]);
+	states->texture = sfTexture_createFromFile("grass_side.png", NULL);
+	states->blendMode = sfBlendAlpha;
+	states->shader = NULL;
+	states->transform = sfTransform_Identity;
+	sfRenderWindow_drawVertexArray(this->window, this->vertices, states);
 }
 
 void draw_line(struct sfml_renderer_system *this, struct vertex_component \
@@ -45,10 +79,10 @@ void draw_line(struct sfml_renderer_system *this, struct vertex_component \
 
 	if (y < 0 || x < 0)
 		return;
-	if (!info->vertices[y] || info->vertices[y][x] == INT32_MAX)
+	if (!info->vertices[y] || info->vertices[y][x].z == INT32_MAX)
 		return;
-//	printf("Drawing line for vertex at %d, %d\n", x, y);
-	vert1->position = get_tile_coords(x, y, info->vertices[y][x]);
+	//printf("Drawing line for vertex at %d, %d\n", x, y);
+	vert1->position = get_tile_coords(info->vertices[y][x].x, info->vertices[y][x].y, info->vertices[y][x].z);
 	sfRenderWindow_drawVertexArray(this->window, this->vertices, NULL);
 }
 
@@ -56,13 +90,20 @@ void draw_line(struct sfml_renderer_system *this, struct vertex_component \
 void sfmlrenderer_draw_tilemap(struct sfml_renderer_system *this, \
 struct vertex_component *info)
 {
-	sfVertex *vert0 = sfVertexArray_getVertex(this->vertices, 0);
+    sfVertex *vert0 = sfVertexArray_getVertex(this->vertices, 0);
+	int corners[3];
+	int i;
 
-    for (int y = 0; info->vertices[y]; y++) {
-        for (int x = 0; info->vertices[y][x] != INT32_MIN; x++) {
-            vert0->position = get_tile_coords(x, y, info->vertices[y][x]);
-            draw_line(this, info, x, y - 1);
-            draw_line(this, info, x - 1, y);
-        }
+	for (i = 0; info->map[i].corners[0]->z != INT32_MIN; i++);
+	//my_printf("zdzdzdzd\n");
+    for (i--; i > 0; i--) {
+		//my_printf("mdr %i\n", i);
+		corners[0] = info->map[i].corners[0]->x;
+		corners[1] = info->map[i].corners[0]->y;
+		corners[2] = info->map[i].corners[0]->z;
+        //if (!info->map[i].corners[0]->y)
+       //     continue; 
+        vert0->position = get_tile_coords(corners[0], corners[1], corners[2]);
+        draw_tile(this, info, info->map[i]);
     }
 }
