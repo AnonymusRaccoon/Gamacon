dfda973a2846c4942f68fe4cb246b9fdc4e5d6f6
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-03-10 17:42:24 +0100

    moving vertex files in isometry folder

---
+++ src/components/isometry/vertex_data_loader.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** Gamacon
+** File description:
+** data_loader
+*/
+
+#include "engine.h"
+#include "data.h"
+#include "xml.h"
+#include "components/transform_component.h"
+#include "components/renderer.h"
+#include "components/fixed_to_cam_component.h"
+#include "components/tag_component.h"
+#include "systems/sfml_renderer_system.h"
+#include "ui.h"
+#include <malloc.h>
+#include "prefab.h"
