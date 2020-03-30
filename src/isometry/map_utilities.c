d34b02f7b61e621a208a91efbb024095c3a8845f
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-03-30 16:58:47 +0200

    premerge

--- src/isometry/map_utilities.c
+++ src/isometry/map_utilities.c
@@ -5,3 +5,16 @@
 ** map utilities
 */
 
+#include "vertex_component.h"
+#include "renderer.h"
+
+void set_entity_ptr_to_tile(gc_entity *entity, struct tile *tile)
+{
+    struct renderer *renderer;
+    tile->entity = entity;
+
+    if (!entity)
+        return;
+    renderer = entity->get_component(entity, "renderer");
+    renderer->is_visible = false;
+}
