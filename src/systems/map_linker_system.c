ef2e30570a67c2c64064afa8fc25ceacf3a5c198
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-03-31 23:41:51 +0200

    added the average altitude to the tile entity and redraw from back to front

---
+++ src/systems/map_linker_system.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2020
+** my_rpg	
+** File description:
+** map_linker_system.c
+*/
+
+#include "components/map_linker.h"
+#include "system.h"
+#include "map_utils.h"
+#include "components/transform_component.h"
+
+void map_linker_update_entity(gc_engine *engine, gc_entity *entity, \
+struct tile *tile, gc_vector2 map_offset) {
+    struct map_linker *link = GETCMP(entity, map_linker);
+    struct transform_component *tra = GETCMP(entity, transform_component);
+    struct vertex *c = tile->corners[0];
+    int z = 0;
+
+    if (!link)
+        return;
+    link->tile = tile;
+    if (!tra)
+        return;
+    for (int i = 0; i < 4; i++)
+        z += tile->corners[i]->z;
+    tra->position = gc_vector2_from_coords(c->x + 1.2, c->y + 1.2, z / 4);
+    tra->position.x += map_offset.x;
+    tra->position.y += map_offset.y;
+    tra->position.y *= -1;
+}
