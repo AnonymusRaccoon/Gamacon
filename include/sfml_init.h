92f864187c97aa6c0dfbfbc43a3c8eb0a6d26e3b
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-04-14 18:28:23 +0200

    adding defines for the texture height and width (for isometry rendering)

---
+++ include/sfml_init.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** My3D
+** File description:
+** sfml_init
+*/
+
+#ifndef MY_RPG_SFML_INIT_H
+#define MY_RPG_SFML_INIT_H
+
+#define TEXTURE_WIDTH 16
+#define TEXTURE_HEIGHT 16
+
+#endif //MY_RPG_SFML_INIT_H
