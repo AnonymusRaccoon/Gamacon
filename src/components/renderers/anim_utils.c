8493449a020472127da0468bf1159c33aab0d5b7
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-28 16:03:58 +0200

    Adding checks to the set anim

--- src/components/renderers/anim_utils.c
+++ src/components/renderers/anim_utils.c
@@ -13,6 +13,8 @@ void rend_set_anim(struct renderer *rend, const char *name)
 {
     gc_animholder *holder = (gc_animholder *)rend->data;
 
+    if (rend->type != GC_ANIMREND || !holder)
+        return;
     if (holder->current && !my_strcmp(holder->current->name, name))
         return;
     for (int i = 0; i < holder->animcount; i++) {
@@ -22,5 +24,4 @@ void rend_set_anim(struct renderer *rend, const char *name)
             return;
         }
     }
-    my_printf("Gamacon: unknown animation %s.\n", name);
 }
